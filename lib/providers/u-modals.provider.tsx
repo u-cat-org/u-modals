f71fc8f0076e3c34bfaae9c9fa5ea98db79657f0
opoltoratskyi <oleks.poltoratskyi@icloud.com>
2024-06-28 16:30:00 +0200

    Implement base flow with reactstrap + bootstrap dependencies added.

--- lib/providers/u-modals.provider.tsx
+++ lib/providers/u-modals.provider.tsx
@@ -1,1 +1,54 @@
-// TODO. Implement modals provider here
+import React, {
+  createContext,
+  useContext, useMemo,
+  useState
+} from 'react';
+import { UModal } from '../components';
+import { IModalConfig, IUModalContext, TResolveFunc } from '../u-modals.typings.ts';
+
+
+const DEFAULT_U_MODAL_CONTEXT: IUModalContext = {
+  showModal: () => Promise.resolve(true)
+};
+
+const UModalContext = createContext(DEFAULT_U_MODAL_CONTEXT);
+
+export const useModals = () => {
+  return useContext(UModalContext);
+};
+
+export const UModalProvider = ({ children }: React.PropsWithChildren) => {
+  const [ modals, setModals ] = useState<IModalConfig[]>([]);
+
+  function showModal(header: string, body: string): Promise<boolean> {
+    let resolveFunc: TResolveFunc;
+
+    const promise = new Promise<boolean>((resolve) => {
+      resolveFunc = resolve as TResolveFunc;
+    });
+
+    setModals((modals) => [ ...modals, { header, body, resolveFunc } ]);
+
+    return promise;
+  }
+
+  function onModalClose(result: boolean, index: number): void {
+    modals[index].resolveFunc(result);
+
+    // @ts-expect-error There is no way to skip first argument `m` declaring
+    setModals((modals) => [ ...modals.filter((m, i) => i !== index) ]);
+  }
+
+  const memoizedValue = useMemo(() => {
+    return { showModal };
+  }, [ modals ]);
+
+  return <UModalContext.Provider value={ memoizedValue }>
+    { children }
+    { modals.map((m, i) =>
+      <UModal key={ i } header={ m.header } body={ m.body }
+              onClose={ (result) => onModalClose(result, i) }/>
+    ) }
+  </UModalContext.Provider>;
+};
+
--- lib/u-modals.typings.ts
+++ lib/u-modals.typings.ts
@@ -1,1 +1,20 @@
-// TODO. add types here
+// Provider
+export interface IUModalContext {
+  showModal: (header: string, body: string) => Promise<boolean>;
+}
+
+export type TResolveFunc = (result: boolean) => boolean
+
+
+export interface IModalConfig {
+  header: string;
+  body: string;
+  resolveFunc: TResolveFunc
+}
+
+
+export interface IUModalProps {
+  header: string;
+  body: string;
+  onClose?: (result: boolean) => void;
+}
